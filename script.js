4c968d349c7f09f54613cab025a1fc580e67c07c
Milad Mohamadi <milad.mohamadi1990@gmail.com>
2023-10-09 15:05:51 +0330

    Refactor codebase for improved readability and maintainability
    In this commit, I've made several enhancements to the codebase:
    
    - Removed unnecessary sections from the project
    - Refactored code for better performance and organization

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let dx = 10;
 let dy = 0;
 
 let score = 0;
-let changingDirection = false;
 document.addEventListener("keydown", changeDirection);
 function changeDirection(event) {
   const LEFT_KEY = 37;
@@ -24,24 +23,25 @@ function changeDirection(event) {
   const DOWN_KEY = 40;
 
   if (changeDirection) return;
+
   changeDirection = true;
   const keyPressed = event.keyCode;
-  if (keyPressed == LEFT_KEY && dx !== 10) {
+  if (keyPressed === LEFT_KEY && dx !== 10) {
     dx = -10;
     dy = 0;
   }
 
-  if (keyPressed == RIGHT_KEY && dx !== -10) {
+  if (keyPressed === RIGHT_KEY && dx !== -10) {
     dx = 10;
     dy = 0;
   }
 
-  if (keyPressed == UP_KEY && dy !== 10) {
+  if (keyPressed === UP_KEY && dy !== 10) {
     dx = 0;
     dy = -10;
   }
 
-  if (keyPressed == DOWN_KEY && dy !== -10) {
+  if (keyPressed === DOWN_KEY && dy !== -10) {
     dx = 0;
     dy = 10;
   }
@@ -62,9 +62,10 @@ function main() {
 
 function didGameEnd() {
   for (let i = 1; i < snake.length; i++) {
-    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) return true;
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) return true;
     1;
   }
+
   const leftWall = snake[0].x < 0;
   const rightWall = snake[0].x > gameCanvas.width - 10;
   const topWall = snake[0].y < 0;
@@ -83,8 +84,8 @@ let createFood = () => {
   foodX = randomNumber(0, gameCanvas.width - 10);
   foodY = randomNumber(0, gameCanvas.height - 10);
   snake.forEach((snakePart) => {
-    if (snakePart.x == foodX && snakePart.y == foodY) {
-      createFood;
+    if (snakePart.x === foodX && snakePart.y === foodY) {
+      createFood();
     }
   });
 };
@@ -93,7 +94,7 @@ let advanceSnake = () => {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
 
   snake.unshift(head);
-  if (head.x == foodX && head.y == foodY) {
+  if (head.x === foodX && head.y === foodY) {
     score += 10;
     document.getElementById("score").innerHTML = score;
     createFood();
@@ -123,5 +124,6 @@ snake.forEach((snakePart) => {
   ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
   ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 });
+
 createFood();
 main();
